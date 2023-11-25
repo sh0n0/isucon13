1b8bf4acc86f70d9aea80f0276ee8f6b539a09d8
sh0n0 <44708388+sh0n0@users.noreply.github.com>
2023-11-25 17:21:15 +0900

    N+1

--- go/livestream_handler.go
+++ go/livestream_handler.go
@@ -494,22 +494,10 @@ func fillLivestreamResponse(ctx context.Context, tx *sqlx.Tx, livestreamModel Li
 		return Livestream{}, err
 	}
 
-	var livestreamTagModels []*LivestreamTagModel
-	if err := tx.SelectContext(ctx, &livestreamTagModels, "SELECT * FROM livestream_tags WHERE livestream_id = ?", livestreamModel.ID); err != nil {
+	var tags []Tag
+	err = tx.SelectContext(ctx, &tags, "SELECT tags.id, tags.name FROM livestream_tags INNER JOIN tags ON livestream_tags.tag_id = tags.id WHERE livestream_id = ?", livestreamModel.ID)
+	if err != nil {
 		return Livestream{}, err
-	}
-
-	tags := make([]Tag, len(livestreamTagModels))
-	for i := range livestreamTagModels {
-		tagModel := TagModel{}
-		if err := tx.GetContext(ctx, &tagModel, "SELECT * FROM tags WHERE id = ?", livestreamTagModels[i].TagID); err != nil {
-			return Livestream{}, err
-		}
-
-		tags[i] = Tag{
-			ID:   tagModel.ID,
-			Name: tagModel.Name,
-		}
 	}
 
 	livestream := Livestream{
